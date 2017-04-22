67e3333647e59450ac4160fc8d2c898f4cf0447e
akshayc04 <achougul@syr.edu>
2017-04-22 19:55:25 -0400

    sound effects

--- app/src/main/java/com/gofit/CategoryActivity.java
+++ app/src/main/java/com/gofit/CategoryActivity.java
@@ -2,6 +2,7 @@ package com.gofit;
 
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.media.MediaPlayer;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -16,6 +17,8 @@ import com.google.android.gms.location.places.ui.PlacePicker;
 
 public class CategoryActivity extends AppCompatActivity {
 
+    MediaPlayer mediaPlayer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +27,8 @@ public class CategoryActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                mediaPlayer = MediaPlayer.create(CategoryActivity.this, R.raw.clicks);
+                mediaPlayer.start();
                Intent intent = new Intent(getApplicationContext(),BeginnerActivity.class);
                 startActivity(intent);
             }
